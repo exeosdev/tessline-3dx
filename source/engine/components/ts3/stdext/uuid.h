fcfc15d0181ed7bca24c129e6c01d0ed7af03e6a
Mateusz Grzejek <mateusz@exeosdev.com>
2021-08-31 04:28:51 +0200

    Updated /samples CMakeLists to properly include the new test project, updated UUID utility implementation.

--- source/engine/components/ts3/stdext/uuid.h
+++ source/engine/components/ts3/stdext/uuid.h
@@ -4,21 +4,34 @@
 namespace ts3
 {
 
-	inline std::string getUUIDString( const std::string & input )
+	inline std::string getUUIDString( const std::string & pInput, char pLeftDelim = '{' )
 	{
 		std::string result;
 
-		auto lbpos = input.find_first_of( '{', 0 );
-		auto rbpos = input.find_first_of( '}', lbpos );
+		char rightDelim = 0;
 
-		if( ( lbpos != std::string::npos ) && ( rbpos!= std::string::npos ) )
+		switch( pLeftDelim )
 		{
-			auto uuidstr = input.substr( lbpos, rbpos - lbpos + 1 );
-			// {xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx}
-			if( uuidstr.length() == 38 )
-			{
-				result = std::move( uuidstr );
-			}
+		    case '{': rightDelim = '}'; break;
+		    case '[': rightDelim = ']'; break;
+		    case '<': rightDelim = '>'; break;
+		    case '$': rightDelim = '$'; break;
+		}
+
+		if( rightDelim != 0 )
+		{
+		    auto lbPos = pInput.find_last_of( pLeftDelim, 0 );
+		    auto rbPos = pInput.find_first_of( rightDelim, lbPos );
+
+		    if( ( lbPos != std::string::npos ) && ( rbPos != std::string::npos ) )
+		    {
+		        auto uuidstr = pInput.substr( lbPos, rbPos - lbPos + 1 );
+                // #xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx#
+		        if( uuidstr.length() == 38 )
+		        {
+		            result = std::move( uuidstr );
+		        }
+		    }
 		}
 
 		return result;
