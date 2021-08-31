d8b7f33bb6f8b73dadc48778992f387eda1b9d23
Mateusz Grzejek <mateusz@exeosdev.com>
2021-08-31 04:24:35 +0200

    Added new simple test app for system testing.

---
+++ source/samples/sysTest/sysTest-main.cpp
@@ -0,0 +1,19 @@
+
+#include <ts3/system/displayManager.h>
+#include <ts3/system/displayDriver.h>
+#include <ts3/system/sysContextNative.h>
+
+int main( int pArgc, const char ** pArgv )
+{
+    ts3::system::SysContextCreateInfo sysCtxCreateInfo;
+    sysCtxCreateInfo.nativeParams.appExecModuleHandle = ::GetModuleHandleA( nullptr );
+    sysCtxCreateInfo.flags = 0;
+
+    auto sysCtx = ts3::system::creCreateSystemContext( sysCtxCreateInfo );
+    auto dmgr = new ts3::system::DisplayManager( sysCtx );
+    auto ddrv = dmgr->createDisplayDriver( ts3::system::EDisplayDriverType::Generic );
+
+    ddrv->initializeDisplayConfiguration();
+
+    return 0;
+}
