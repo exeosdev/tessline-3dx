729ed609ef4ddca6d36ee25456616c9d4ac83a41
Mateusz Grzejek <mateusz@exeosdev.com>
2021-09-06 11:41:11 +0200

    Updated Win32 FileManager, added generic file opnening, added public method for temp file name generation.

--- source/engine/components/ts3/system/internal/platform/shared/wnf/wnfCommon.cpp
+++ source/engine/components/ts3/system/internal/platform/shared/wnf/wnfCommon.cpp
@@ -1,16 +1,23 @@
 
 #include <ts3/system/prerequisites.h>
+#include "wnfCommon.h"
 #include <comdef.h>
 
 #if( TS3_PCL_TARGET_OS & TS3_PCL_TARGET_FLAG_OS_WINFAMILY )
 namespace ts3::system
 {
 
-    std::string wnfQueryHresultErrorMessage( HRESULT pHResult )
+	namespace platform
 	{
-		_com_error comError{ pHResult };
-		_bstr_t comErrorString{ comError.ErrorMessage() };
-		return { static_cast<const char*>( comErrorString ) };
+
+        std::string wnfQueryCOMErrorMessage( HRESULT pHResult )
+        {
+            _com_error comError{ pHResult };
+            _bstr_t comErrorString{ comError.ErrorMessage() };
+            return { static_cast<const char*>( comErrorString ) };
+        }
+
+
 	}
 
 } // namespace ts3::system
