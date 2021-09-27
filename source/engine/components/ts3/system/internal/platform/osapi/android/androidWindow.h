5a34156cf841c67f5c0ce45aaff28d56bd2386c0
Mateusz Grzejek <mateusz@exeosdev.com>
2021-09-27 09:24:25 +0200

    [Android] Window implementation (wrapper on to of ANativeWindow).

--- source/engine/components/ts3/system/internal/platform/osapi/android/androidWindow.h
+++ source/engine/components/ts3/system/internal/platform/osapi/android/androidWindow.h
@@ -2,15 +2,22 @@
 #ifndef __TS3_SYSTEM_PLATFORM_OSAPI_ANDROID_WINDOW_H__
 #define __TS3_SYSTEM_PLATFORM_OSAPI_ANDROID_WINDOW_H__
 
-#include "androidEventCore.h"
+#include "androidEventSystem.h"
 
-namespace ts3
+namespace ts3::system
 {
 
-	struct WindowNativeData : public EventSourceNativeData
+	struct WindowManagerNativeData : public AndroidNativeDataCommon
 	{
 	};
 
-}
+	struct WindowNativeData : public EventSourceNativeData
+	{
+		ANativeWindow * aNativeWindow = nullptr;
+	};
+
+	TS3_SYSTEM_API_NODISCARD math::Size2u nativeAndroidQueryNativeWindowSize( ANativeWindow * pANativeWindow );
+
+} // namespace ts3::system
 
 #endif // __TS3_SYSTEM_PLATFORM_OSAPI_ANDROID_WINDOW_H__
