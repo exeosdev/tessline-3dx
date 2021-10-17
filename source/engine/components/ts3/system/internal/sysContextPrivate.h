3b40c3c076437da333b6d1ceb774b5240b5d8fd9
Mateusz Grzejek <mateusz@exeosdev.com>
2021-10-17 17:03:06 +0200

    [SystemRefactor] Updated sysTest-gpuapi sample, added private header for SysContext.

---
+++ source/engine/components/ts3/system/internal/sysContextPrivate.h
@@ -0,0 +1,12 @@
+
+#ifndef __TS3_SYSTEM_INTERNAL_SYS_CONTEXT_PRIVATE_H__
+#define __TS3_SYSTEM_INTERNAL_SYS_CONTEXT_PRIVATE_H__
+
+#include <ts3/system/sysContext.h>
+#include <thread>
+
+namespace ts3::system
+{
+} // namespace ts3::system
+
+#endif // __TS3_SYSTEM_INTERNAL_SYS_CONTEXT_PRIVATE_H__
