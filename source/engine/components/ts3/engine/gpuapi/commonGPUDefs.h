a4c76830709e0c2e04fcee4417cb44b88523c94e
Mateusz Grzejek <mateusz@exeosdev.com>
2022-03-25 03:58:16 +0100

    Build fixes for GpuAPI:: component.

--- source/engine/components/ts3/engine/gpuapi/commonGPUDefs.h
+++ source/engine/components/ts3/engine/gpuapi/commonGPUDefs.h
@@ -29,7 +29,7 @@ namespace ts3
 
 		GPUBufferRef( std::nullptr_t )
 		: buffer{ nullptr }
-		, memoryRegion{ 0, gpuapi::cxGPUMemorySizeMax }
+		, memoryRegion{ 0, gpuapi::CX_GPU_MEMORY_SIZE_MAX }
 		{}
 
 		explicit operator bool() const
