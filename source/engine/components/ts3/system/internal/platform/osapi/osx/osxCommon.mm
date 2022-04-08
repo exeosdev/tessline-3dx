bb72eca1df2753b6213d1eac2de28394e57bd06e
Mateusz Grzejek <mateusz@exeosdev.com>
2022-04-08 05:20:06 +0200

    Updated common OSX definitions, added dedicated, base NS header.

--- source/engine/components/ts3/system/internal/platform/osapi/osx/osxCommon.mm
+++ source/engine/components/ts3/system/internal/platform/osapi/osx/osxCommon.mm
@@ -1,5 +1,7 @@
 
-#include "osxCommon.h"
+#include "osxSysContext.h"
+#include "nsOSXApplicationProxy.h"
+#import <AppKit/NSNibLoading.h>
 
 #if( TS3_PCL_TARGET_SYSAPI == TS3_PCL_TARGET_SYSAPI_OSX )
 namespace ts3::system
@@ -7,6 +9,90 @@ namespace ts3::system
 
 	namespace platform
 	{
+
+		OSXSharedData & osxGetOSXSharedData( SysContext & pSysContext )
+		{
+			auto * osxSysContext = pSysContext.queryInterface<OSXSysContext>();
+			return osxSysContext->mNativeData.osxSharedData;
+		}
+
+		OSXSharedData & osxGetOSXSharedData( OSXSysContext & pSysContext )
+		{
+			return pSysContext.mNativeData.osxSharedData;
+		}
+
+		bool osxNibLoadMenuNibFile()
+		{
+		@autoreleasepool
+		{
+			const auto cvAppKitVersion = floor( NSAppKitVersionNumber );
+
+			if( cvAppKitVersion >= NSAppKitVersionNumber10_8 )
+			{
+				NSBundle * mainBundle = [NSBundle mainBundle];
+				NSDictionary * bundleInfoDict = [mainBundle infoDictionary];
+				if( bundleInfoDict != nil )
+				{
+					NSString * mainNibFleNameStr = [bundleInfoDict valueForKey:@"NSMainNibFile"];
+
+					if( mainNibFleNameStr != nil )
+					{
+						if( [mainBundle loadNibNamed:mainNibFleNameStr owner:[NSApplication sharedApplication] topLevelObjects:nil] )
+						{
+							return true;
+						}
+					}
+					else
+					{
+						NSString * mainMenuNibResource = [mainBundle pathForResource:@"MainMenu" ofType:@"nib"];
+						if( mainMenuNibResource != nil )
+						{
+							if( [mainBundle loadNibNamed:@"MainMenu" owner:[NSApplication sharedApplication] topLevelObjects:nil] )
+							{
+								return true;
+							}
+						}
+
+					}
+				}
+			}
+
+			return false;
+		}
+		}
+
+		void osxNibCreateDefaultApplicationMenu()
+		{
+		@autoreleasepool
+		{
+			if( NSApp == nil )
+			{
+				return;
+			}
+
+			NSMenu * mainAppMenuBar = [[NSMenu alloc] init];
+			[NSApp setMainMenu:mainAppMenuBar];
+
+			auto * appMenuItem = [mainAppMenuBar addItemWithTitle:@"" action:nullptr keyEquivalent:@""];
+			auto * appMenu = [[NSMenu alloc] init];
+			[appMenuItem setSubmenu:appMenu];
+
+			auto * servicesMenuItem = [appMenu addItemWithTitle:@"Services" action:nullptr keyEquivalent:@""];
+			auto * servicesMenu = [[NSMenu alloc] init];
+			[NSApp setServicesMenu:servicesMenu];
+			[servicesMenuItem setSubmenu:servicesMenu];
+
+			auto * windowMenuItem = [mainAppMenuBar addItemWithTitle:@"" action:nullptr keyEquivalent:@""];
+			auto * windowMenu = [[NSMenu alloc] initWithTitle:@"Window"];
+			[NSApp setWindowsMenu:windowMenu];
+			[windowMenuItem setSubmenu:windowMenu];
+
+			SEL setAppleMenuSelector = NSSelectorFromString(@"setAppleMenu:");
+			[NSApp performSelector:setAppleMenuSelector withObject:appMenu];
+
+			[mainAppMenuBar release];
+		}
+		}
 
 		const char * osxQueryCGErrorMessage( CGError pCGError )
 		{
