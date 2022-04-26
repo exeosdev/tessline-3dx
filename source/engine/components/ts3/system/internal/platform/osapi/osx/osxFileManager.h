ad98ace63602d002f6e5c949a8350d7b060ab19b
Mateusz Grzejek <mateusz@exeosdev.com>
2022-04-26 13:20:13 +0200

    Updated .gitignore, added missing FileManager for OSX.

---
+++ source/engine/components/ts3/system/internal/platform/osapi/osx/osxFileManager.h
@@ -0,0 +1,8 @@
+
+#ifndef __TS3_SYSTEM_PLATFORM_OSAPI_OSX_FILE_MANAGER_H__
+#define __TS3_SYSTEM_PLATFORM_OSAPI_OSX_FILE_MANAGER_H__
+
+#include "osxCommon.h"
+#include "../../shared/posix/posixFileManager.h"
+
+#endif // __TS3_SYSTEM_PLATFORM_OSAPI_OSX_FILE_MANAGER_H__
